e21c856b7188061dd939f57d190c68a25a23f78b
Olivier CHURLAUD <olivier@churlaud.com>
2015-07-16 15:27:16 +0200

    add Matlab binaries

--- examples/ccBSC_example.m
+++ examples/ccBSC_example.m
@@ -1,7 +1,8 @@
 clear all
 path('../bin/',path);
 
-p = 0.2
+p = 0:0.01:0.5;
+N = 10^5;
 
 A = [0,1;0,0];
 B = [1,0];
@@ -11,38 +12,42 @@ D = [1,1];
 [fwd, bwd] = ccInitialize(A,B,C,D);
 
 s0 = 0;
-
-N = 10000;
-
 seq = round(rand(1,N));
 
 [c, sN] = ccEncode(fwd,seq,s0);
 
 %% BSC
 randomNoise = rand(1,N*2);
-channel = zeros(1,N*2);
+for idx = 1:numel(p)
+    
+    channel = zeros(1,N*2);
+    channel(randomNoise < p(idx)) = 1;
 
-channel(randomNoise < p) = 1;
+    c_b = reshape(de2bi(c)',1,[]);
+    tmp = c_b;
 
-c_b = reshape(de2bi(c)',1,[]);
-tmp = c_b;
+    c_b(randomNoise <= p(idx) & tmp == 1) = 0;
+    c_b(randomNoise <= p(idx) & tmp == 0) = 1;
+    %% Channel
+    Bmetric = zeros(length(c_b),2);
 
-c_b(randomNoise < p & tmp == 1) = 0;
-c_b(randomNoise < p & tmp == 0) = 1;
-%% Channel
-Bmetric = zeros(length(c_b),2);
-
-for i = 1:length(c_b)
-    if c_b(i) == 1
-        Bmetric(i,1) = log(p);
-        Bmetric(i,2) = log(1-p);
-    else
-        Bmetric(i,1) = log(1-p);
-        Bmetric(i,2) = log(p);
+    for i = 1:length(c_b)
+        if c_b(i) == 1
+            Bmetric(i,1) = log(p(idx));
+            Bmetric(i,2) = log(1-p(idx));
+        else
+            Bmetric(i,1) = log(1-p(idx));
+            Bmetric(i,2) = log(p(idx));
+        end
     end
-end
-%% Decode
-[m , ch] = ccDecode(bwd,length(c),Bmetric,s0,sN);
+    %% Decode
+    [m , ch] = ccDecode(bwd,length(c),Bmetric,s0,sN);
 
-%% BER
-display(['Result (message): ',num2str(length(find(m ~= seq))/length(m)) '% of error']);
+    %% BER
+    BER(idx) = length(find(m ~= seq))/length(m);
+end
+
+semilogy(p,BER);
+xlabel('Error probability');
+ylabel('BER')
+title('BSC Channel')
